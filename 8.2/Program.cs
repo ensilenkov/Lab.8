a0abc5ff29baa69adf9b5f319a9095969dab8ec5
Евгений Силенков <ensilenkov@gmail.com>
2021-12-15 14:51:30 +0700

    Добавьте файлы проекта.

---
+++ 8.2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _8._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "‪Logs.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+            StreamWriter logs = new StreamWriter(path);
+            Random r = new Random();
+            for (int i=0; i<10; i++)
+            {
+                logs.WriteLine(r.Next(0,100));
+            }
+            logs.Close();
+            string s = "";
+            int sum = 0;
+            StreamReader sr = new StreamReader(path);
+            //Console.WriteLine(sr.ReadToEnd());
+           for (int i=0; i<10;i++)
+            {
+                s = sr.ReadLine();
+                sum = sum + int.Parse(s);
+            }
+           Console.WriteLine(sum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Lab.8/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/79138/Documents/Архив";
+            Console.WriteLine("СПИСОК КАТАЛОГОВ ПАПКИ АРХИВ");
+            DirectoryInfo dir = new DirectoryInfo(path);
+            foreach (DirectoryInfo d in dir.GetDirectories())
+
+            {
+                Console.WriteLine(d.Name);
+                Console.WriteLine("СПИСОК ФАЙЛОВ");
+                foreach (FileInfo f in d.GetFiles())
+                {
+                    Console.WriteLine(f.Name);
+                }
+                
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
